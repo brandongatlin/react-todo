b24dc9a17584f7cfe73922e77cb447698cd7fc9a
brandongatlin <brandongatlin1981@me.com>
2019-10-24 16:11:13 -0500

    making test graphql rest query and logging person name

--- brandon-todo/src/App.js
+++ brandon-todo/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import ApolloClient from "apollo-boost";
+import ApolloClient from "apollo-client";
+import { gql } from "apollo-boost";
+
 import { ApolloProvider } from '@apollo/react-hooks';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { RestLink } from 'apollo-link-rest';
 
 import AllTodos from './Components/Views/AllTodos';
 
-const restLink = new RestLink({ uri: "https://practiceapi.devmountain.com/api/tasks/" });
-
+// const restLink = new RestLink({ uri: "https://practiceapi.devmountain.com/api/tasks/" });
+const restLink = new RestLink({ uri: "https://swapi.co/api/" });
 
 const client = new ApolloClient({
   link: restLink,
@@ -20,9 +22,22 @@ const client = new ApolloClient({
   }
 });
 
+const query = gql`
+  query luke {
+    person @rest(type: "Person", path: "people/1/") {
+      name
+    }
+  }
+`;
+
+// Invoke the query and log the person's name
+client.query({ query }).then(response => {
+  console.log(response.data.person.name);
+});
+
 const App = () => (
   <ApolloProvider client={client}>
-    <AllTodos />
+    {/* <AllTodos /> */}
   </ApolloProvider>
 );
 
