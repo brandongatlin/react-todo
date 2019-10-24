333c7c58609ae99ff719a81c5c3626c9a35d50e0
brandongatlin <brandongatlin1981@me.com>
2019-10-24 08:15:24 -0500

    keys and props all working with checkboxes

--- brandon-todo/src/Components/Checkbox/index.js
+++ brandon-todo/src/Components/Checkbox/index.js
@@ -3,19 +3,18 @@ import Switch from '@material-ui/core/Switch';
 
 
 const Checkbox = (props) => {
+
 const [checked, setChecked] = React.useState(props.checked);
 
     return(
-    <Switch
-    checked={checked}
-    onChange={(e) => {
-        console.log(e.target);
-        setChecked(!checked);
-        console.log(checked);
-    }}
-    value={checked}
-    inputProps={{ 'aria-label': 'secondary checkbox' }}
-    />
+        <Switch
+            checked={checked}
+            onChange={(e) => {
+                setChecked(!checked);
+            }}
+            value={checked}
+            inputProps={{ 'aria-label': 'secondary checkbox' }}
+        />
     );
 }
 
--- brandon-todo/src/Components/Views/AllTodos/index.js
+++ brandon-todo/src/Components/Views/AllTodos/index.js
@@ -3,26 +3,26 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
 import Card from '../../Card';
-import Checkbox from '../../Checkbox';
+// import Checkbox from '../../Checkbox';
 
 const data = [
     {
         id : 1,
         title : "title here",
         text : "first",
-        done : false
+        checked : false
     },
     {
         id : 2,
         title : "title here",
         text : "second",
-        done : true
+        checked : true
     },
     {
         id : 3,
         title : "title here",
         text : "third",
-        done : false
+        checked : false
     },
 ]
 
@@ -31,10 +31,9 @@ const AllTodos = (props)=> {
         <Grid container spacing={3}>
             {data.map((task)=> {
                 return (
-                    <div key={task.id}>
-                        <Card  title={task.title} text={task.text}/>
-                        <Checkbox checked={task.done} value={task.done}/>
-                    </div>
+                    <Grid key={task.id} item sm={12}>
+                        <Card title={task.title} text={task.text} checked={task.checked}/>
+                    </Grid>
                 );
             })}
         </Grid>
